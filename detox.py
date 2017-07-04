05d42e11116b72d7c08cfe887731a8a6bdeb0481
Kevin Cole <dc.loco@gmail.com>
2017-07-04 17:51:08 -0400

    - Catwalk down the site looking for HTML files

--- detox.py
+++ detox.py
@@ -2,29 +2,29 @@
 # -*- coding: utf-8 -*-
 #
 #  detox.py
-#  
+#
 #  Detox uses Beautiful Soup to attempt removing all the Microsoft
 #  Front Page toxins from the National Hospital directory tree. Note
 #  the use of the word "attempt"...
 #
 #  Copyright 2017 Kevin Cole <kevin.cole@novawebcoop.org> 2017.07.01
-#  
+#
 #  This program is free software; you can redistribute it and/or
 #  modify it under the terms of the GNU General Public License as
 #  published by the Free Software Foundation; either version 2 of
 #  the License, or (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public
 #  License along with this program; if not, write to the Free
 #  Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 #  Boston, MA 02110-1301, USA.
-#  
-#  
+#
+#
 
 
 from __future__ import print_function
@@ -33,7 +33,9 @@ from os.path    import expanduser      # Cross-platform home directory finder
 import os
 import sys
 
-from bs4 import BeautifulSoup
+import re                              # Regular expressions
+from   bs4     import BeautifulSoup    # HTML parser
+from   catwalk import fetch            # Fetch files with regex name match
 
 __appname__    = "detox"
 __author__     = "Kevin Cole"
@@ -59,7 +61,12 @@ def main():
                   __agency__,
                   __email__))
 
-    # [Your code here]
+    root = "."
+    pattern = re.compile(r".*\.htm.*")
+
+    for nextfile in fetch(root, pattern):
+        print(nextfile)
+#       pass
 
     return 0
 
